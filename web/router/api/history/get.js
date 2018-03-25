8738dca530c23e20820a79888aece7ffeadd8004
Jake Wang <jaceyshome@gmail.com>
2018-03-26 01:30:19 +1100

    fix: returns null on finding history breaks the history/get

--- web/router/api/history/get.js
+++ web/router/api/history/get.js
@@ -3,7 +3,7 @@ const controller = require('./../../../components/history/controller');
 
 async function get(ctx, next) {
     let data = await controller.get(ctx);
-    if(data.err){
+    if(data && data.err){
         ctx.res.badRequest({message: data.err.message});
     } else {
         ctx.res.success(data, "Success");
