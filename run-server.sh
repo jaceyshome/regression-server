2c17d985d4f0d2d570229aa22ff97a6886b01072
Jake Wang <jaceyshome@gmail.com>
2018-09-23 22:19:50 +1000

    resolve conflicts, change config for the production environment

--- run-server.sh
+++ run-server.sh
@@ -2,13 +2,13 @@
 
 #Production server log directory and data directory
 #For local development, don't share the log and data as different system has different location
-#logDirectory="/tmp/regression-test-server";
-#dataDirectory="/tmp/regression-test-server";
+logDirectory="/tmp/regression-test-server";
+dataDirectory="/tmp/regression-test-server";
 
 docker run  -d \
         --name regression-test-server  \
-       # -v ${dataDirectory}/datastore:/app/datastore \
-       # -v ${logDirectory}/logs:/app/logs \
+        -v ${dataDirectory}/datastore:/app/datastore \
+        -v ${logDirectory}/logs:/app/logs \
         -p 7071:7071 \
         -p 9651:9651 \
         jacobwang05/regression-test-server
